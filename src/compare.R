ab6f7ed0059782311ec94669ebe8a6a9fd3790cc
MinaAlmasi <mina.almasi00@gmail.com>
2024-04-10 15:52:52 +0200

    fix: change paths

--- src/compare.R
+++ src/compare.R
@@ -1,8 +1,8 @@
 pacman::p_load(here, loo)
 
 # read in data
-weighted_samples <- readRDS(here::here("data", "model_quality", "weighted_samples.rds"))
-simple samples <- readRDS(here::here("data", "model_quality", "simple_samples.rds"))
+weighted_samples <- readRDS(here::here("data", "real_samples", "weighted_samples_real.rds"))
+simple_samples <- readRDS(here::here("data", "real_samples", "simple_samples_real.rds"))
 
 # get loo
 weighted_samples_loo <- weighted_samples$loo(save_psis = TRUE, cores=3)
@@ -10,3 +10,4 @@ simple_samples_loo <- simple_samples$loo(save_psis = TRUE, cores=3)
 
 # compare 
 loo_compare(weighted_samples_loo, simple_samples_loo)
+
