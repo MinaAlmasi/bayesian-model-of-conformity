703555e3379205bfe166bd33fe6bf91e89b1de82
MinaAlmasi <mina.almasi00@gmail.com>
2024-04-10 16:44:23 +0200

    feat: add elpd plot

--- src/compare.R
+++ src/compare.R
@@ -11,3 +11,20 @@ simple_samples_loo <- simple_samples$loo(save_psis = TRUE, cores=3)
 # compare 
 loo_compare(weighted_samples_loo, simple_samples_loo)
 
+# plot differential elpd
+n_data_points <- nrow(weighted_samples_loo$pointwise)
+
+elpd <- tibble(
+    n = seq(n_data_points),
+    diff_elpd = simple_samples_loo$pointwise[, "elpd_loo"]  - weighted_samples_loo$pointwise[, "elpd_loo"] 
+)
+
+elpd_plot <- ggplot(elpd, aes(x = n, y = diff_elpd)) +
+    geom_point(alpha = .1) +
+    #xlim(.5,1.01) +
+    #ylim(-1.5,1.5) +
+    geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
+    theme_bw()
+
+# save plot
+ggsave(here::here("plots", "diff_elpd.jpg"), elpd_plot, width = 20, height = 10, units = "cm")
