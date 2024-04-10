1eee3fd20f0966f9d4568204bd3d5440726860a1
MinaAlmasi <mina.almasi00@gmail.com>
2024-04-10 14:10:34 +0200

    feat: run both simple and weighted bayes

--- src/run_model.R
+++ src/run_model.R
@@ -1,8 +1,6 @@
 pacman::p_load(tidyverse, here, cmdstanr, rstan, posterior)
 
-stan_filepath = here::here("stan", "weighted_bayes.stan")
-
-fit_model <- function(df){
+fit_model <- function(df, stan_filepath){
     # prepare the data
     data <- list("Trials" = length(df$Trials), 
                 "FirstRating" = df$FirstRating,
@@ -28,7 +26,16 @@ fit_model <- function(df){
     return(samples)
 }
 
-df_filepath = here::here("data", "simulated_data_weighted.csv")
-df <- read_csv(df_filepath)
+## WEIGHTED BAYES ## 
+weighted_stanpath = here::here("stan", "weighted_bayes.stan")
+weighted_df <- read_csv(here::here("data", "simulated_data_weighted.csv"))
 
-samples <- fit_model(df)
+# fit 
+weighted_samples <- fit_model(weighted_df, weighted_stanpath)
+
+## SIMPLE BAYES ## 
+simple_stanpath = here::here("stan", "simple_bayes.stan")
+simple_df <- read_csv(here::here("data", "simulated_data_simple.csv"))
+
+# fit
+simple_samples <- fit_model(simple_df, simple_stanpath)
